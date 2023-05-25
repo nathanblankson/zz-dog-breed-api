bf1c20a89cd829b76845eb34178cdab5f3c887b0
Nathan Blankson <nathan@sourcebreaker.com>
2023-05-26 00:04:57 +0100

    Added images

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('/api');
+  app.useStaticAssets('images');
   app.enableCors();
 
   await app.listen(3000);
